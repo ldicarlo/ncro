e21f186ced2db11e01ff1004b09bcf9b005942af
NotAShelf <raf@notashelf.dev>
2026-03-06 16:41:01 +0300

    nix: init package at 0.1.0
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I1fee4c7eaefdf72bf3f1b6f6866e47006a6a6964

--- nix/package.nix
+++ nix/package.nix
@@ -1,11 +1,26 @@
-{buildGoModule}:
+{
+  lib,
+  buildGoModule,
+}:
 buildGoModule {
-  pname = "sample-go";
-  version = "0.0.1";
+  pname = "ncro";
+  version = "0.1.0";
 
-  src = ../.;
+  src = let
+    fs = lib.fileset;
+    s = ../.;
+  in
+    fs.toSource {
+      root = s;
+      fileset = fs.unions [
+        (s + /cmd)
+        (s + /internal)
+        (s + /go.mod)
+        (s + /go.sum)
+      ];
+    };
 
-  vendorHash = "";
+  vendorHash = "sha256-suI8EAgRFG7BDJP2aqLWsej6FTP+OrEsmxRyV5hkKK0=";
 
   ldflags = ["-s" "-w"];
 }
