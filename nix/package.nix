ecfc4743c62b4137870d1a67ede8e92494f97722
NotAShelf <raf@notashelf.dev>
2026-05-26 16:01:48 +0300

    nix: derive package version from workspace; fix `checkFlags`
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6aadab8efb4abd1b4d3b9b965eac55096a6a6964

--- nix/package.nix
+++ nix/package.nix
@@ -2,37 +2,37 @@
   lib,
   rustPlatform,
   pkg-config,
-}:
-rustPlatform.buildRustPackage (finalAttrs: {
-  pname = "ncro";
-  version = "2.0.0";
+}: let
+  cargoTOML = (lib.importTOML ../Cargo.toml).workspace.package;
+in
+  rustPlatform.buildRustPackage (finalAttrs: {
+    pname = "ncro";
+    inherit (cargoTOML) version;
 
-  src = let
-    fs = lib.fileset;
-    s = ../.;
-  in
-    fs.toSource {
-      root = s;
-      fileset = fs.unions [
-        (s + /ncro)
-        (s + /crates)
-        (s + /Cargo.toml)
-        (s + /Cargo.lock)
-      ];
+    src = let
+      fs = lib.fileset;
+      s = ../.;
+    in
+      fs.toSource {
+        root = s;
+        fileset = fs.unions [
+          (s + /ncro)
+          (s + /crates)
+          (s + /Cargo.toml)
+          (s + /Cargo.lock)
+        ];
+      };
+
+    # relies on CA certificates
+    checkFlags = ["--skip=ncro::tests::ema_and_status_progression"];
+
+    cargoLock.lockFile = "${finalAttrs.src}/Cargo.lock";
+    nativeBuildInputs = [pkg-config];
+
+    meta = {
+      homepage = "https://github.com/feel-co/ncro";
+      license = lib.licenses.eupl12;
+      mainProgram = "ncro";
+      maintainers = with lib.maintainers; [NotAShelf];
     };
-
-  # relys on ca certificates
-  checkFlags = ''
-      "--skip=ncro::tests::ema_and_status_progression"
-    '';
-
-  cargoLock.lockFile = "${finalAttrs.src}/Cargo.lock";
-  nativeBuildInputs = [pkg-config];
-
-  meta = {
-    homepage = "https://github.com/feel-co/ncro";
-    license = lib.licenses.eupl12;
-    mainProgram = "ncro";
-    maintainers = with lib.maintainers; [NotAShelf];
-  };
-})
+  })
